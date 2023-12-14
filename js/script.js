8d7f9b9fb1af00c575c9a3fade1ff24e279bed73
Chaidar Saad <ariecakestore@gmail.com>
2023-12-14 11:00:21 +0700

    feature kritik dan saran

--- js/script.js
+++ js/script.js
@@ -57,3 +57,18 @@ window.onclick = (e) => {
     itemDetailModal.style.display = 'none';
   }
 };
+
+window.addEventListener("load", function () {
+  const form = document.getElementById("my-form");
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const data = new FormData(form);
+    const action = e.target.action;
+    fetch(action, {
+      method: "POST",
+      body: data,
+    }).then(() => {
+      alert("Pesan berhasil terkirim! Silahkan tunggu admin membalas");
+    });
+  });
+});
